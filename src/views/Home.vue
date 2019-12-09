f47954bce4bd359b83e4a47d62cbf0601c4291ac
mengzhiai <458677565@qq.com>
2019-12-09 11:40:51 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
+<!--
+ * @Author: your name
+ * @Date: 2019-12-09 11:28:25
+ * @LastEditTime: 2019-12-09 11:40:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-demo\src\views\Home.vue
+ -->
 <template>
   <div class="home">
+    1
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
